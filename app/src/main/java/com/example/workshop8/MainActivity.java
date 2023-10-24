2b72cec1fd5cefda987c1a468506015d35816c75
HowardASong <zi-hao.song@outlook.com>
2023-10-24 13:56:46 -0600

    Customer

--- app/src/main/java/com/example/workshop8/MainActivity.java
+++ app/src/main/java/com/example/workshop8/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.workshop8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvCustomer;
+    Button btAdd,btnSave,btnDelete;
+    ArrayAdapter<Customer> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_customer);
+
     }
 }
