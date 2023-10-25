f812bf1354ad928e1b52600637c385a1c959623f
HowardASong <zi-hao.song@outlook.com>
2023-10-25 17:51:13 -0600

    CustomerActivaty

--- app/src/main/java/com/example/workshop8/CustomerActivity.java
+++ app/src/main/java/com/example/workshop8/CustomerActivity.java
@@ -8,16 +8,32 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.concurrent.Executors;
 
 public class CustomerActivity extends AppCompatActivity {
     Button btnAdd;
     ListView lvCustomers;
+
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,34 +43,69 @@ public class CustomerActivity extends AppCompatActivity {
         btnAdd = findViewById(R.id.btnAdd);
         lvCustomers = findViewById(R.id.lvCustomers);
 
+        requestQueue = Volley.newRequestQueue(this);
+
+        Executors.newSingleThreadExecutor().execute(new GetCustomers());
+
         lvCustomers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext() , CustomerDetailsActivity.class);
+                ListViewCustomer agent = (ListViewCustomer) lvCustomers.getAdapter().getItem(position);
+                Intent intent = new Intent(getApplicationContext(), CustomerDetailsActivity.class);
+                intent.putExtra("listviewcustomer", agent);
+                startActivity(intent);
             }
         });
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CustomerDetailsActivity.class);
+                startActivity(intent);
             }
         });
+
+
+
     }
 
-    /*private class CustomerDisplay implements Runnable(){
-
+    class GetCustomers implements Runnable{
         @Override
         public void run() {
-            try {
-                URL url = new URL("http://localhost:8080/Workshop7-1.0-SNAPSHOT/customers/getallcustomers");
-                InputStream is = url.openStream();
-                Bitmap bitmap = BitmapFactory.decodeStream(is);
-                runOnUiThread(new Runnable() {
-                });
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }*/
-//    }
+            StringBuffer buffer = new StringBuffer();
+            String url = "localhost:8080/Workshop7-1.0-SNAPSHOT/api/getallcustomers";
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
+                @Override
+                public void onResponse(String response) {
+                    ArrayAdapter<ListViewCustomer> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1);
+                    try {
+                        JSONArray jsonArray = new JSONArray(response);
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject cust = jsonArray.getJSONObject(i);
+                            ListViewCustomer customer = new ListViewCustomer(cust.getInt("customerId"), cust.getString("custFirstName"), cust.getString("custLastName"));
+                            adapter.add(customer);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    final ArrayAdapter<ListViewCustomer> finalAdapter = adapter;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            lvCustomers.setAdapter(finalAdapter);
+                        }
+                    });
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    VolleyLog.wtf(error.getMessage(), "utf-8");
+                }
+            });
+
+            requestQueue.add(stringRequest);
+        }
+   }
 }
---
+++ app/src/main/java/com/example/workshop8/ListViewCustomer.java
@@ -0,0 +1,39 @@
+package com.example.workshop8;
+
+import java.io.Serializable;
+
+public class ListViewCustomer implements Serializable {
+    private int CustomerId;
+    private String CustFirstName;
+    private String CustLastName;
+
+    public ListViewCustomer(int customerId, String custFirstName, String custLastName) {
+        CustomerId = customerId;
+        CustFirstName = custFirstName;
+        CustLastName = custLastName;
+    }
+
+    public int getCustomerId() {
+        return CustomerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        CustomerId = customerId;
+    }
+
+    public String getCustFirstName() {
+        return CustFirstName;
+    }
+
+    public void setCustFirstName(String custFirstName) {
+        CustFirstName = custFirstName;
+    }
+
+    public String getCustLastName() {
+        return CustLastName;
+    }
+
+    public void setCustLastName(String custLastName) {
+        CustLastName = custLastName;
+    }
+}
